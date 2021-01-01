62d4e54b47a76f69251bf641104df1000f3c821b
heet <54179610+heet05@users.noreply.github.com>
2021-01-01 22:25:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vadecha_ne_vacho/PrettyTime.java
@@ -0,0 +1,14 @@
+package com.example.vadecha_ne_vacho;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class PrettyTime {
+    public PrettyTime(Locale locale) {
+
+    }
+
+    public String format(Date date) {
+        return null;
+    }
+}
