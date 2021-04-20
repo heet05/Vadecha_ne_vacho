9a2f515b6d91d10e2b6a3e77e9aacfe23a0beead
heet <heetvadecha99@gmail.com>
2021-04-20 17:39:14 +0530

    add new about us

---
+++ app/src/main/java/com/heet/vadecha_ne_vacho/AboutUsActivity.java
@@ -0,0 +1,14 @@
+package com.heet.vadecha_ne_vacho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
